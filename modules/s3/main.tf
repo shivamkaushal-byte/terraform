b7b688d8893aee0d9915c8c88f93807fb59a487a
unknown <practicedevops60@gmail.com>
2023-02-16 00:32:13 +0530

    first commit

---
+++ modules/s3/main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
+}
